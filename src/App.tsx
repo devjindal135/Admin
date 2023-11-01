46eee34425ac363910a2f24b16a28cc3992c1372
devjindal135 <devjindal135@gmail.com>
2023-11-01 14:13:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Leaves = lazy(() => import("./pages/Leaves"));
+const Customers = lazy(() => import("./pages/Customers"));
+const NewProduct = lazy(() => import("./pages/management/NewProduct"));
+const ProductManagement = lazy(
+  () => import("./pages/management/ProductManagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/management/TransactionManagement")
+);
+
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Link to="/admin/dashboard">
+                <button>Visit Dashboard</button>
+              </Link>
+            }
+          />
+
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/Leaves" element={<Leaves />} />
+
+          {/* Charts */}
+
+          
+
+          {/* Management */}
+          <Route path="/admin/product/new" element={<NewProduct />} />
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
